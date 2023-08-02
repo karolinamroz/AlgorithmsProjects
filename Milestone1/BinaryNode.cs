c0aec0aaaf7bb12d323345cf2a4dc822f9688988
karolinamroz <75311341+karolinamroz@users.noreply.github.com>
2023-08-02 10:38:30 +0100

    Add files via upload

---
+++ Milestone1/BinaryNode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    internal class BinaryNode <T>
+    {
+        internal T Value { get; set; }
+        internal BinaryNode<T> LeftChild, RightChild;
+
+        internal BinaryNode(T value)
+        {
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+        }
+        internal void AddLeft(BinaryNode<T> child)  {   LeftChild = child; }
+
+        internal void AddRight(BinaryNode<T> child) {  RightChild = child; }
+
+        public override string ToString()
+        {
+            string result = string.Format("{0}:", Value);
+
+            if (LeftChild == null)
+                result += " null";
+            else
+                result += string.Format(" {0}", LeftChild.Value);
+
+            if (RightChild == null)
+                result += " null";
+            else
+                result += string.Format(" {0}:", RightChild.Value);
+
+            return result;
+        }
+    }
+}
---
+++ Milestone1/NaryNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    internal class NaryNode <T>
+    {
+        internal T Value { get; set; }
+        internal List <NaryNode<T>> Children;
+
+        internal NaryNode(T value)
+        {
+            Value = value;
+            Children = new List<NaryNode<T>>(); 
+        }
+        internal void AddChild(NaryNode<T> child)
+        {
+            Children.Add(child);
+        }
+        public override string ToString()
+        {
+            string result = string.Format("{0}:", Value);
+            foreach (NaryNode<T> child in Children)
+                result += string.Format(" {0}", child.Value);
+            return result;
+        }
+    }
+}
